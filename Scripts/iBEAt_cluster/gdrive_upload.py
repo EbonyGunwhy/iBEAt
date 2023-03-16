310bafe259bce875bbe8ebc708da3312a86ce2a4
EbonyGunwhy <100941618+EbonyGunwhy@users.noreply.github.com>
2023-03-16 13:42:33 +0000

    Update gdrive_upload.py

--- Scripts/iBEAt_cluster/gdrive_upload.py
+++ Scripts/iBEAt_cluster/gdrive_upload.py
@@ -20,7 +20,7 @@ def Gdrive_upload(dbdicomPath, filename_log, KeyPath):
   upload_file_list = [filename_log, dbdicomPath + '.zip']
 
   for upload_file in upload_file_list:
-    gfile = drive.CreateFile({'title':os.path.basename(upload_file) ,'parents': [{'id': '1-3DEQEFg0TVax4swwMHLmsLUf1aGoUQH'}]})
+    gfile = drive.CreateFile({'title':os.path.basename(upload_file) ,'parents': [{'id': '1bj6yrtn8PkzogeyDVxT3ueml0cQwFUIx'}]})
     # Read file and set it as the content of this instance.
     gfile.SetContentFile(upload_file)
     gfile.Upload(param={'supportsTeamDrives': True}) # Upload the file.
